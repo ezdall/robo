1947ba2f324fa319ae12e937d8c9f262fb843311
ezdall <67018806+ezdall@users.noreply.github.com>
2022-10-07 16:05:26 +0800

    add ErrorBoundry, tachyons for css, fix data error

---
+++ src/components/error-boundry.comp.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+
+export default class ErrorBoundry extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      hasError: false
+    };
+  }
+
+  componentDidCatch() {
+    this.setState({ hasError: true });
+  }
+
+  render() {
+    const { hasError } = this.state;
+    const { children } = this.props;
+
+    if (hasError) {
+      return <h1>Oooops. That is not good!</h1>;
+    }
+    return children;
+  }
+}
--- src/index.jsx
+++ src/index.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-//
+// data
 import { robotData } from './robots';
 
+import 'tachyons';
 // import './bootstrap.min.css';
 // import './index.css';
 //
 import App from './containers/App';
 
-ReactDOM.render(<App robotData={robotData} />, document.getElementById('root'));
+ReactDOM.render(<App robots={robotData} />, document.getElementById('root'));
