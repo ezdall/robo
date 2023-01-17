578ab65d285cd13308ebe3ed54c6cf271240ce06
ezdall <67018806+ezdall@users.noreply.github.com>
2023-01-17 13:46:53 +0800

    fix errorboundry

--- src/components/error-boundry.comp.jsx
+++ src/components/error-boundry.comp.jsx
@@ -8,8 +8,16 @@ export default class ErrorBoundry extends React.Component {
     };
   }
 
-  componentDidCatch() {
+  // static getDerivedStateFromError(error) {
+  // Update state so next render shows fallback UI.
+  // return { error: error };
+  // }
+
+  componentDidCatch(error, info) {
     this.setState({ hasError: true });
+
+    // Log the error to an error reporting service
+    // logErrorToExampleService(error, info);
   }
 
   render() {
@@ -17,7 +25,7 @@ export default class ErrorBoundry extends React.Component {
     const { children } = this.props;
 
     if (hasError) {
-      return <h1>Oooops. That is not good!</h1>;
+      return <h1 className="red">Something broke!..</h1>;
     }
     return children;
   }
